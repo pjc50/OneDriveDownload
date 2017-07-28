aae70e7ad98cf6f641cab092d1da1d6a0ad91fc8
Peter Clay <pjclay+github@gmail.com>
2017-07-28 21:38:21 +0100

    Initial commit

---
+++ OneDriveDownload/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace OneDriveDownload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSync = new System.Windows.Forms.Button();
+            this.progressLabel = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnPickFolder = new System.Windows.Forms.Button();
+            this.lblFolderTarget = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnSignOut = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.overwriteAll = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnSync
+            // 
+            this.btnSync.Location = new System.Drawing.Point(100, 11);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.Size = new System.Drawing.Size(75, 23);
+            this.btnSync.TabIndex = 4;
+            this.btnSync.Text = "Sync";
+            this.btnSync.UseVisualStyleBackColor = true;
+            this.btnSync.Click += new System.EventHandler(this.btnSync_Click);
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.AutoSize = true;
+            this.progressLabel.Location = new System.Drawing.Point(12, 37);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(99, 13);
+            this.progressLabel.TabIndex = 5;
+            this.progressLabel.Text = "Press sync to begin";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 68);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(638, 268);
+            this.treeView1.TabIndex = 6;
+            // 
+            // btnPickFolder
+            // 
+            this.btnPickFolder.Location = new System.Drawing.Point(569, 12);
+            this.btnPickFolder.Name = "btnPickFolder";
+            this.btnPickFolder.Size = new System.Drawing.Size(81, 22);
+            this.btnPickFolder.TabIndex = 7;
+            this.btnPickFolder.Text = "Pick folder...";
+            this.btnPickFolder.UseVisualStyleBackColor = true;
+            this.btnPickFolder.Click += new System.EventHandler(this.btnPickFolder_Click);
+            // 
+            // lblFolderTarget
+            // 
+            this.lblFolderTarget.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblFolderTarget.AutoSize = true;
+            this.lblFolderTarget.Location = new System.Drawing.Point(394, 21);
+            this.lblFolderTarget.Name = "lblFolderTarget";
+            this.lblFolderTarget.Size = new System.Drawing.Size(99, 13);
+            this.lblFolderTarget.TabIndex = 8;
+            this.lblFolderTarget.Text = "(path goes      here)";
+            this.lblFolderTarget.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(181, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 9;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnSignOut
+            // 
+            this.btnSignOut.Location = new System.Drawing.Point(262, 12);
+            this.btnSignOut.Name = "btnSignOut";
+            this.btnSignOut.Size = new System.Drawing.Size(82, 23);
+            this.btnSignOut.TabIndex = 10;
+            this.btnSignOut.Text = "Sign out";
+            this.btnSignOut.UseVisualStyleBackColor = true;
+            this.btnSignOut.Click += new System.EventHandler(this.btnSignOut_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(12, 11);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(82, 23);
+            this.btnLogin.TabIndex = 11;
+            this.btnLogin.Text = "Sign in";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // overwriteAll
+            // 
+            this.overwriteAll.AutoSize = true;
+            this.overwriteAll.Location = new System.Drawing.Point(372, 48);
+            this.overwriteAll.Name = "overwriteAll";
+            this.overwriteAll.Size = new System.Drawing.Size(146, 17);
+            this.overwriteAll.TabIndex = 12;
+            this.overwriteAll.Text = "Overwrite all (WARNING)";
+            this.overwriteAll.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 348);
+            this.Controls.Add(this.overwriteAll);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnSignOut);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lblFolderTarget);
+            this.Controls.Add(this.btnPickFolder);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.btnSync);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSync;
+        private System.Windows.Forms.Label progressLabel;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnPickFolder;
+        private System.Windows.Forms.Label lblFolderTarget;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnSignOut;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.CheckBox overwriteAll;
+    }
+}
+
---
+++ OneDriveDownload/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.OneDrive.Sdk;
+using Microsoft.OneDrive.Sdk.WindowsForms;
+
+namespace OneDriveDownload
+{
+    public partial class Form1 : Form
+    {
+        IOneDriveClient oneDriveClient;
+        private const string MsaClientId = "000000004817F8B9";
+        private const string MsaReturnUrl = "https://login.live.com/oauth20_desktop.srf";
+        private static readonly string[] Scopes = { "onedrive.readonly", "wl.offline_access", "wl.signin" };
+        //private static readonly string[] Scopes = { "onedrive.readonly"};
+        private static readonly string[] skipDirs = { };
+
+        //string localfileroot = "C:\\TEMP";
+        string localfileroot = "E:\\Laura OneDrive Backup";
+
+        CancellationTokenSource cancelToken;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblFolderTarget.Text = localfileroot;
+        }
+
+        private async void btnSync_Click(object sender, EventArgs e)
+        {
+            btnSync.Enabled = false;
+            await listRootFiles();
+            btnSync.Enabled = true;
+            progressLabel.Text = "finished or cancelled.";
+        }
+
+        // From MS sample code
+        private async Task SignIn()
+        {
+            if (this.oneDriveClient == null)
+            {
+                this.oneDriveClient =
+                    OneDriveClient.GetMicrosoftAccountClient(
+                        MsaClientId,
+                        MsaReturnUrl,
+                        Scopes,
+                        webAuthenticationUi: new FormsWebAuthenticationUi());
+                    
+            }
+
+            try
+            {
+                if (!this.oneDriveClient.IsAuthenticated)
+                {
+                    await this.oneDriveClient.AuthenticateAsync();
+                }
+
+
+                //UpdateConnectedStateUx(true);
+            }
+            catch (OneDriveException exception)
+            {
+                // Swallow authentication cancelled exceptions
+                if (!exception.IsMatch(OneDriveErrorCode.AuthenticationCancelled.ToString()))
+                {
+                    if (exception.IsMatch(OneDriveErrorCode.AuthenticationFailure.ToString()))
+                    {
+                        MessageBox.Show(
+                            "Authentication failed",
+                            "Authentication failed",
+                            MessageBoxButtons.OK);
+
+                        var httpProvider = this.oneDriveClient.HttpProvider as HttpProvider;
+                        httpProvider.Dispose();
+                        this.oneDriveClient = null;
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+        } // end signin
+
+        private void validateOrCreateDirectory(string path)
+        {
+            string localDir = Path.Combine(localfileroot, path);
+            if(!System.IO.Directory.Exists(localDir))
+            {
+                System.IO.Directory.CreateDirectory(localDir);
+            }
+        }
+
+        private async Task validateOrDownloadFile(string path, Item item)
+        {
+            string localFile = Path.Combine(localfileroot,path);
+            bool doDownload = false;
+            DateTime itemDateUTC = item.FileSystemInfo.LastModifiedDateTime.GetValueOrDefault().UtcDateTime;
+            progressLabel.Text = "validating file " + localFile + " last modified " + itemDateUTC.ToString();
+            if(System.IO.File.Exists(localFile))
+            {
+                FileInfo f = new FileInfo(localFile);
+                System.Console.WriteLine("exists, size " + f.Length + " expected " + item.Size);
+                if(f.LastWriteTimeUtc == itemDateUTC
+                    && f.Length != 0
+                   /* f.Length == item.Size*/)
+                {
+                    System.Console.WriteLine("date time size match for " + localFile+ " " + item.Size + " bytes at " + itemDateUTC);
+                }
+                else
+                {
+                    if (item.Size > 2147483648L)
+                    {
+                        System.Console.WriteLine("too damn large");
+                        doDownload = false;
+                    }
+                    else if (item.Size == 0)
+                    {
+                        doDownload = false;
+                    }
+                    else if (overwriteAll.Checked)
+                    {
+                        doDownload = true;
+                    }
+                    else
+                    {
+                        DialogResult yn = MessageBox.Show(
+                                "Date/Time/Size mismatch for " + localFile +
+                                "\n OneDrive: " + item.Size + " bytes at " + itemDateUTC +
+                                "\n Local: " + f.Length + " bytes at " + f.LastWriteTimeUtc +
+                                "\nOverwrite this file?",
+                                "File mismatch",
+                                MessageBoxButtons.YesNo);
+                        if (yn == DialogResult.Yes)
+                            doDownload = true;
+                    }
+                }
+            }
+            else
+            {
+                doDownload = true;
+                System.Console.WriteLine("does not exist");
+            }
+
+            if(doDownload)
+            {
+                progressLabel.Text = "downloading file " + localFile;
+                try
+                {
+                    using (FileStream newFile = System.IO.File.Create(localFile, 1024 * 1024))
+                    {
+                        using (var contentStream = await oneDriveClient
+                                      .Drive
+                                      .Items[item.Id]
+                                      .Content
+                                      .Request()
+                                      .GetAsync())
+                        {
+                            Task dl = contentStream.CopyToAsync(newFile);
+                            dl.Wait(); // fixme cancel token?
+                            newFile.Close();
+                        }
+                    }
+                    System.IO.File.SetLastWriteTimeUtc(localFile, itemDateUTC);
+                    System.IO.File.SetCreationTimeUtc(localFile, itemDateUTC);
+                    System.Console.WriteLine("finished downloading " + localFile);
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine("Exception " + e.Message + " while downloading " + localFile);
+                    FileInfo fi = new FileInfo(localFile);
+                    if(fi.Length == 0)
+                        System.IO.File.Delete(localFile);
+                }
+            }
+        }
+
+        private async Task retrieveChildren(string path, string rootId, TreeNodeCollection inTree)
+        {
+            validateOrCreateDirectory(path);
+            TreeNodeCollection subTree = /*path.Length > 0 ? inTree.Add(path).Nodes :*/ inTree;
+            progressLabel.Text = "retrieving children of " + rootId + " at " + path;
+            List<Item> childItems;
+            var rootDir = await oneDriveClient.Drive
+                            .Items[rootId]
+                            .Children
+                            .Request()
+                            .GetAsync();
+            childItems = rootDir.ToList();
+            while(rootDir.NextPageRequest != null)
+            {
+                if (cancelToken.IsCancellationRequested) return;
+                rootDir = await rootDir.NextPageRequest.GetAsync();
+                childItems = childItems.Concat(rootDir).ToList();
+            }
+            foreach (var child in childItems)
+            {
+                if (cancelToken.IsCancellationRequested) return;
+                string childPath = path + child.Name;
+
+                if (child.Folder != null)
+                {
+                    if (skipDirs.Contains(child.Name))
+                    {
+                        System.Console.WriteLine("Skipping " + childPath);
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("Folder " + childPath);
+                        await retrieveChildren(childPath + "\\", child.Id, subTree);
+                    }
+                }
+                else
+                {
+                    System.Console.WriteLine("File " + childPath);
+                    await validateOrDownloadFile(childPath, child);
+                }
+            }
+        }
+
+        private async Task listRootFiles()
+        {
+            var rootItem = await oneDriveClient
+                             .Drive
+                             .Root
+                             .Request()
+                             .GetAsync();
+            await retrieveChildren("", rootItem.Id, treeView1.Nodes);
+        }
+
+        private void btnPickFolder_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lblFolderTarget.Text = folderBrowserDialog1.SelectedPath;
+                localfileroot = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            cancelToken.Cancel();
+        }
+
+        private void btnSignOut_Click(object sender, EventArgs e)
+        {
+            if (oneDriveClient != null && oneDriveClient.IsAuthenticated)
+            {
+                    oneDriveClient.SignOutAsync();
+            }
+        }
+
+        private async void btnLogin_Click(object sender, EventArgs e)
+        {
+            cancelToken = new CancellationTokenSource();
+            await SignIn();
+        }
+    }
+}
